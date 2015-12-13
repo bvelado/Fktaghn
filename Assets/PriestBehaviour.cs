c1fa271fe242d55ed0740346b8cab349c20e45d2
thebatoust <thebatoust@gmail.com>
2015-12-13 03:20:12 +0100

    Commythocondrie

--- Assets/PriestBehaviour.cs
+++ Assets/PriestBehaviour.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using DG.Tweening;
 
 public class PriestBehaviour : MonoBehaviour {
 
@@ -8,13 +8,38 @@ public class PriestBehaviour : MonoBehaviour {
         Patrol, Attack
     }
 
+    PriestState state;
+
+    public Transform[] patrolWaypoints;
+    Vector3[] waypointsPositions;
+    public float patrolSpeed;
+    int waypointIndex;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+        state = PriestState.Patrol;
+        waypointIndex = 0;
+        waypointsPositions = new Vector3[patrolWaypoints.Length];
+        for(int i = 0; i < patrolWaypoints.Length; i++)
+        {
+            waypointsPositions[i] = patrolWaypoints[i].position;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(state == PriestState.Patrol)
+        {
+            transform.DOMove(new Vector3(3, 0, 0), patrolSpeed);
+            if (Vector3.SqrMagnitude(waypointsPositions[waypointIndex] - transform.position) < 0.0001)
+            {
+                waypointIndex++;
+                if(waypointIndex-1 > waypointsPositions.Length)
+                {
+                    waypointIndex = 0;
+                }
+            }
+        }
 	}
 }
