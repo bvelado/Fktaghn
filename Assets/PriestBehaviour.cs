53bf0a4dd07621b3bc3c980b7078ed542abbb30e
thebatoust <thebatoust@gmail.com>
2015-12-13 01:45:03 +0100

    Priest Behaviour

---
+++ Assets/PriestBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PriestBehaviour : MonoBehaviour {
+
+    enum PriestState
+    {
+        Patrol, Attack
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
